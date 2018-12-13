75e9ac3f7dbeae6654facf250d6a2b92c1ad8efa
Somashekhar31 <17311a05r5@sreenidhi.edu.in>
2018-12-13 15:31:27 +0530

    resolve 1

--- src/lab/exp3/scraplab.py
+++ src/lab/exp3/scraplab.py
@@ -80,7 +80,7 @@ while sectionNumber<=len(sectionno):
 tag1 = str(tagger[0].text)	
 tag1 = tag1.strip()
 if tag1 == 'Prerequisite S/W':
-tag1 = 'Prerequisites'
+    tag1 = 'Prerequisites'
 att = ''+'experiment-article-section-'+str(sectionNumber)+'-content'
 	tagger = soup.findAll('div', attrs={'id':att,'class':'content'})
 	
